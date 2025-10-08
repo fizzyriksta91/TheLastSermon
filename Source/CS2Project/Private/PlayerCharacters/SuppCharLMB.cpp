2bebae5170f398716b20e6780eba5b8155ec910a
Insomniac2212 <lioneldavidtt@gmail.com>
2025-10-08 14:05:44 +0100

    Support Character added
    Support character added

---
+++ Source/CS2Project/Private/PlayerCharacters/SuppCharLMB.cpp
@@ -0,0 +1,35 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlayerCharacters/SuppCharLMB.h"
+
+#include "PlayerCharacters/Components/SuppCharCompLMB.h"
+
+
+// Sets default values
+ASuppCharLMB::ASuppCharLMB()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	SuppCharLMB = CreateDefaultSubobject<USuppCharCompLMB>(TEXT("Support Character Component"));
+}
+
+// Called when the game starts or when spawned
+void ASuppCharLMB::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ASuppCharLMB::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void ASuppCharLMB::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
---
+++ Source/CS2Project/Public/PlayerCharacters/SuppCharLMB.h
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BaseCharacter.h"
+#include "SuppCharLMB.generated.h"
+
+UCLASS()
+class CS2PROJECT_API ASuppCharLMB : public ABaseCharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ASuppCharLMB();
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+class USuppCharCompLMB* SuppCharLMB;
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+};
