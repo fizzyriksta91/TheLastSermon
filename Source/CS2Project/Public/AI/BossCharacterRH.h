eb0301ce5ac009cf634c6db5987b6a3aea63b87f
Rikki Hughes <rikki91@live.co.uk>
2025-11-05 12:57:45 +0000

    Setup Boss character stats
    Set up the boss character Stats and collisions

---
+++ Source/CS2Project/Public/AI/BossCharacterRH.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AI/EnemyBaseCharacter.h"
+#include "BossCharacterRH.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CS2PROJECT_API ABossCharacterRH : public AEnemyBaseCharacter
+{
+	GENERATED_BODY()
+	
+};
